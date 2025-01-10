bc262e46e220146d3617ab6d3d9707541983aaa1
fourvrd <kacperpiwowarski@outlook.com>
2025-01-10 20:21:10 +0100

    feat: create dashboard prototype with mock data

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -1,10 +1,17 @@
-import "./App.css";
+import { Navbar } from "./components/Navbar";
+import { DeviceCard } from "./components/DeviceCard";
+import deviceTypes from "./mocks/device_types.json";
 
 function App() {
   return (
     <>
-      <div className="flex place-content-center items-center h-screen border">
-        <p className="text-xl">iot-mqtt-dashboard</p>
+      <div className="">
+        <Navbar />
+        <div className="flex flex-wrap justify-center gap-5 p-10">
+          {deviceTypes.map((device) => (
+            <DeviceCard deviceType={device.type_id} />
+          ))}
+        </div>
       </div>
     </>
   );
---
+++ dashboard/src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+export const Navbar: React.FC = () => {
+  return (
+    <nav className="bg-[#353535] p-5">
+      <div className="flex">
+        <div className="text-white text-lg font-bold">iot-mqtt-dashboard</div>
+      </div>
+    </nav>
+  );
+};
